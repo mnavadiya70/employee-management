b3188bca251e89c251f37dceeb602f8fe203fa79
mnavadiya70 <mnavadiya70@gmail.com>
2020-12-01 18:50:19 +0530

    added material-ui theme

--- src/appRouting.tsx
+++ src/appRouting.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import EmployeePage from './pages/employeePage';
+import TableData from './pages/materialUITable';
 import CreateUpdateEmployee from './pages/createUpdateEmployee';
 
 const routing = () => {
     return (
         <>
-            <Route path="/" exact component={EmployeePage} />
+            <Route path="/" exact component={TableData} />
             <Route path="/create" exact component={CreateUpdateEmployee} />
             <Route path="/edit/:key" exact component={CreateUpdateEmployee} />
         </>
